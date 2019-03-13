c94a1209627845695e59b1ce6ec03da24459ac29
Riley Schippers <02riley.schippers@gulllakecs.org>
2019-03-13 12:40:22 -0400

    First Commit

---
+++ Exam.py
@@ -0,0 +1,21 @@
+#Adding a small greeting function for your name and age
+
+name = input('What is your name?:')
+
+age = -1
+
+try:
+    age = int(input('How old are you?: '))
+except ValueError:
+    print('\n''That isn\'t correct' )
+
+print('\n''Name:', name)
+print('Age:', age)
+#Added A question that asks for your favorite sport and number
+
+sport = input('what is your favorite sport?:')
+
+favorite_number = input('What is your favorite number?')
+
+print('\n''Favorite Sport:', sport)
+print('\n''Favorite Number:', favorite_number)

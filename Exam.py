d5089682f94d8eb85a936d4069719828266bbef0
Riley Schippers <02riley.schippers@gulllakecs.org>
2019-03-15 11:24:47 -0400

    Made a small greetings function for try and except

--- Exam.py
+++ Exam.py
@@ -18,33 +18,3 @@ except ValueError:
 
 print('\n''Name:', name)
 print('Age:', age)
-#Added A question that asks for your favorite sport and number
-
-sport = input('what is your favorite sport?:')
-
-favorite_number = input('What is your favorite number?:')
-
-print('\n''Favorite Sport:', sport)
-print('\n''Favorite Number:', favorite_number)
-
-#Adding A question for your favorite color
-for i in range(1):
-    Color = input('What is your favorite color?:')
-    print('\n''Favorite Color:', Color)
-
-#Added a question for guessing a number one through ten
-my_number = 1
-
-print ("Guess my number of one through ten")
-print ("")
-
-guess = int(input("Enter a guess: "))
-
-
-while guess != my_number:
-    print ("")
-    print ("No")
-    guess = int(input("Enter a guess: "))
-
-print ("")
-print ("Good job you win!")
